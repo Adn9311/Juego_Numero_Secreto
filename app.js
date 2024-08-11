42d5319d5ffc10c4e7cdfa63ce17a19a6c59795d
Adn9311 <zambranos.adan@gmail.com>
2024-08-10 19:32:21 -0600

    Versión Final Juego Alura Latam
    Versión final del juego para adivinar el numero secreto creado en el curso de lógica de programación

---
+++ app.js
@@ -0,0 +1,81 @@
+let numeroSecreto =0;
+let intentos=1;
+let listaNumerosSorteados=[];
+let numeroMaximo = 10;
+
+console.log(numeroSecreto);
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;    
+}
+
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById("valorUsuario").value);
+    
+    console.log(intentos);
+    if(numeroDeUsuario==numeroSecreto){
+       asignarTextoElemento("p", `Acertaste el número ${intentos} ${intentos===1 ? "intento": "intentos"}`); 
+       document.getElementById("reiniciar").removeAttribute("disabled");
+    
+    } else{
+        //el usuario no acertó
+        if(numeroDeUsuario>numeroSecreto){
+            asignarTextoElemento("p","El numero secreto es menor")
+        } else{
+            asignarTextoElemento("p","El numero secreto es mayor")
+        }    
+        intentos++;
+        limpiarCaja();    
+    }
+    
+    return;
+}
+
+
+function limpiarCaja() {
+    let valorCaja = document.querySelector("#valorUsuario").value="";
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado= Math.floor(Math.random()*numeroMaximo)+1;
+    
+    console.log(listaNumerosSorteados)
+    
+    if(listaNumerosSorteados.length==numeroMaximo){
+        asignarTextoElemento("p", "Ya se sortearon todos los números posibles");
+
+    }else { 
+        //si el número generado está incluido en la lista    
+        if(listaNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+        }else{
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales(){
+    asignarTextoElemento("h1","Juego del número secreto");
+    asignarTextoElemento("p", `Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos=1;
+}
+
+
+function reiniciarJuego(){
+    //limpiar caja
+    limpiarCaja();
+
+    //mensaje de bienvenida, numero aleatorio, intentos
+    condicionesIniciales();
+
+    //deshabilitar bton
+    document.querySelector("#reiniciar").setAttribute("disabled", "true");
+}
+
+
+condicionesIniciales();
